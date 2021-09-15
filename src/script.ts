e48eeb186e3318f308dd0ff7052a43754699cc8f
claudegose <84382636+claudegose@users.noreply.github.com>
2021-09-15 15:39:54 +0200

    first commit

---
+++ src/script.ts
@@ -0,0 +1,32 @@
+//todo output js file to index.html
+
+
+(function() {
+    document.getElementById('guess-form').addEventListener("click", compareUserGuess);
+
+    function compareUserGuess (event) {
+        event.preventDefault();
+
+        const secretNumber = generateSecretNumber();
+        const userNumber = (<HTMLInputElement>document.getElementById('guess')).value;
+
+        // @ts-ignore
+        if (secretNumber === userNumber) {
+            alert(`Awesome! You number ${userNumber} was correct. You can be named many things, hungry not being one of them.`);
+        } else {
+            alert(`Bummer... You guessed ${userNumber} and the secret number was ${secretNumber}`);
+        }
+    }
+
+    function generateSecretNumber() {
+        const secretNumber = getRandomArbitrary(1, 22);
+
+        return secretNumber;
+    }
+
+    function getRandomArbitrary(min, max) {
+        return Math.round(Math.random() * (max - min) + min);
+    }
+})
+
+();
